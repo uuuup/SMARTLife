9590b3633a79032517ab5e69fb0c3daa9bc27d3f
uuuup <wangpu19971016@foxmail.com>
2018-04-29 16:59:17 +0800

    在主线程中开子线程来进行socket通信 但目前是服务器可以接受到数据 客户端无法接受返回的数据

--- app/src/main/java/com/example/uuuup/myapplication/TcpManager.java
+++ app/src/main/java/com/example/uuuup/myapplication/TcpManager.java
@@ -3,37 +3,58 @@ package com.example.uuuup.myapplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
+public class TcpManager extends Thread {
+    private String sendData;
+    private String receiveData;
+    private Socket socket;
 
-public class TcpManager {
+    public TcpManager(String string)
+    {
+        System.out.print("socket data: " + string);
+        this.sendData = string;
+    }
 
-    protected void onCreate(Bundle savedInstanceState) {
+    public String getData(){
+        return this.receiveData;
+    }
+
+    @Override
+    public void run() {
+        // 执行完毕后给handler发送一个空消息
         try {
-
-            Socket socket = new Socket("127.0.0.1", 22);
-            //设置10秒之后即认为是超时
+            // 实例化Socket
+            socket = new Socket("211.159.187.82", 20005);
             socket.setSoTimeout(10000);
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
-            String line = br.readLine();
+            System.out.print("new socket!!!!!!!!!");
 
-            br.close();
-            socket.close();
+            //获取输入流
+            BufferedReader buf =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // 获得输出流
+            PrintStream out = new PrintStream(socket.getOutputStream());
+
+            out.print(this.sendData);
+            try {
+                this.receiveData = buf.readLine();
+                System.out.println("receive data"+ this.receiveData);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Time out, No response");
+            }
+            buf.close();
         } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
-            Log.e("UnknownHost", "来自服务器的数据");
             e.printStackTrace();
         } catch (IOException e) {
-            Log.e("IOException", "来自服务器的数据");
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        //handler.sendEmptyMessage(0); //不需要界面变化的话可不写handler
     }
 }
